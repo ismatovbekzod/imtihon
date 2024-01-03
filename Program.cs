757d8c3e55521d9973ff678cf1ca14e3d810e219
Bekzod <64043604+ismatovbekzod@users.noreply.github.com>
2024-01-03 18:39:17 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,54 +1,48 @@
 ﻿using System;
-using System.Net.NetworkInformation;
+using System.Diagnostics;
 
-// Console.Clear();
-
-// Console dan raduisni qiymatini oling va erkanga doiraning yuzi (S=pi*radius^2) va 
-// aylana uzunligini (L=2*pi*radius) erkanga chiqaring.
-// Input: radius=3
-// Output: S=28.2743338823081, L=18.8495559215388
-// 1-vazifa
-// System.Console.Write("Radiusni kiriting : ");
-// string radius = Console.ReadLine()!;
-// int r = Convert.ToInt32(radius);
-// const double pi = 3.14;
-
-// double s = pi*Math.Pow(r,2);
-// double l = 2*pi*r;
-// System.Console.WriteLine("Yuzi : " + s);
-// System.Console.WriteLine("Aylana uzunligi : " + l);
+Console.Clear();
 
 
-//2-vazifa
-// Bir valyutadagi summani sumga aylantiruvchi dastur tuzing. Summa va valyuta kursini 
-// e’lon qiling va konvertatsiya qilingan summani hisoblang. Natijani ekranga chiqaring.
-// Input: qiymat=2, kurs=12400 so’m
-// Output: 24800 so’m
-// ----------------
-// Input: qiymat=7.6, kurs=12400 so’m
-// Output: 94240 so’m
-// System.Console.WriteLine("Dollarni so'mda hisoblab beramiz. ");
-// System.Console.Write("Nechi dollarni hisoblatmoqchisiz : ");
-// string dol = Console.ReadLine()!;
-// double dollar = Convert.ToInt32(dol);
+//1-misol 
+// 1 dan 1000 gacha bo’lgan toq sonlarni yig’indisini ekranga chiqaradigan dastur tuzing. While loop dan foydalaning!
+// int i = 1;
+// int sum = 0;
+// while(i <= 1000)
+// {
+//     if(i % 2 == 1)
+//     {
+//         sum += i;
+//     }
+//     i++;
+// }
+// System.Console.WriteLine(sum);
 
-// int kurs = 12400;
-// System.Console.WriteLine($"Xozirda kurs {kurs} shunca. KURS O'ZGARIB TURISHI MUMKIN !!!");
-// double som = dollar*kurs;
-// System.Console.WriteLine(som);
+//2-misol
+// Array ichidagi eng katta va eng kichik qiymatlarni ko’paytmasini ekranga chiqaradigan dastur tuzing. For loop dan foydalaning!
 
+// int[] arr = {5,3,2,3,4,5,7,9};
+// int min = arr[0];
+// int max = arr[0];
+// for(int i = 0;i < arr.Length;i++)
+// {
+//     if(min > arr[i]){
+//         min = arr[i];
+//     }
+//     if(max < arr[i]){
+//         max = arr[i];
+//     }
+// }
+// System.Console.WriteLine($"Eng kichik son : {min} Eng katta son {max} Ko'paytmasi {min  * max}");
 
+// 3-misol
+// Consolga int tipidagi butun son kiritilgan => int x
+// x! (x factorial) ni hisoblaydigan dastur tuzing.
 
-// 3-vazifa
-// Foydalanuvchining tug’ilgan yilini (int x) consoledan oling. Uning yoshini kunlarda 
-// ifodalang. Kabisa yilini hisobga olmang
-// Input: x=2004
-// Output: 6935
-// -----------------
-// Input: x= 1996
-// Output: 9855
-
-System.Console.Write("yilni kiriting : ");
-string yil = Console.ReadLine()!;
-int yosh = Convert.ToInt32(yil);
-System.Console.WriteLine((2023-yosh)*365);
+System.Console.WriteLine("Nechi sonini factorialni topmoqchisiz :");
+int x = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+for(int i = 1;i <= x;i++){
+    sum *= i;
+}
+System.Console.WriteLine(sum);
